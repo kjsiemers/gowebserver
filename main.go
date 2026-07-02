067f8ba5adb55d01a0c44c1855d11bbf75cde7c3
kjsiemers <kjs@umich.edu>
2026-07-01 20:38:02 -0400

    added healthz and app/assets

--- main.go
+++ main.go
@@ -1,42 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"time"
-        "os"
 )
 
 func main() {
-	m := http.NewServeMux()
+	const filepathRoot = "."
+	const port = "8080"
 
-	m.HandleFunc("/", handlePage)
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	port := os.Getenv("PORT")
-	srv := http.Server{
-		Handler:      m,
-		Addr:         ":" + port,
-		WriteTimeout: 30 * time.Second,
-		ReadTimeout:  30 * time.Second,
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	// this blocks forever, until the server
-	// has an unrecoverable error
-	fmt.Println("server started on", port)
-	err := srv.ListenAndServe()
-	log.Fatal(err)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
 
-func handlePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(200)
-	const page = `<html>
-<head></head>
-<body>
-	<p> Hi Docker, I pushed a new version. </p>
-</body>
-</html>
-`
-	w.Write([]byte(page))
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
